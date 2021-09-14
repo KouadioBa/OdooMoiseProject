9c4caefc872245b3244c51d0f5756614fbed68ff
Abdoulaye BARRY <barryab12@gmail.com>
2021-09-14 10:42:57 +0000

    Remove unused files

--- models/models.py
+++ models/models.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
-
-class OpenMoise(models.Model):
-    _name = 'open.moise'
+# class OpenMoise(models.Model):
+#     _name = 'open.moise'
